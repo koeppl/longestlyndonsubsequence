8ca38028aee9dc6f301e0ad17a20c60ad41ba300
Dominik Köppl <git@dkppl.de>
2022-08-01 16:00:50 +0900

    implement paper algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,219 +1,76 @@
 
 
-
-// const CHR_A : u8 = 'a' as u8;
-// const CHR_B : u8 = 'b' as u8;
-//
-// fn thuemorse(i : u8) -> Vec<u8> {
-//     // if i == 0 { return None }
-//     let n = 1<<i;
-//     let mut str : Vec<u8> = Vec::with_capacity(n);
-//     unsafe { str.set_len(n); }
-//     str[0] = CHR_A;
-//     for j in 0..i {
-//         let powerj = 1<<j;
-//         for k in 0..powerj {
-//             str[powerj+k] = if str[k] == CHR_A { CHR_B } else { CHR_A }; 
-//         }
-//     }
-//     return str
-// }
-
-// fn output_string<W: std::fmt::Write>(f: &mut W, s : &Vec<u8>) -> Result<(), std::fmt::Error> {
-//     for val in s {
-//         f.write_char(if *val == 0 as u8 { 'a' } else { 'b' })?;
-//     }
-//     Ok(())
-// }
-
-extern crate byte_string;
-extern crate bit_vec;
-
 #[macro_use]
 extern crate more_asserts;
 
-use byte_string::ByteString;
+/// reads a file into a u8 vector
+/// - `prefix_length` : the prefix in bytes to read from `filename`. 0 means to read the entire file
+pub fn file2byte_vector(filename: &str, prefix_length : usize) -> Vec<u8> {
+    use std::fs;
+    use std::io::Read;
 
-fn suffixarray(text: & ByteString) -> Vec<usize> {
-    let mut sa = vec![0; text.len()];
-    for i in 0..text.len() {
-        sa[i] = i as usize;
-    }
-    sa.sort_by_key(|x| &text[*x..] );
-    sa
-}
+    let path = std::path::Path::new(filename);
+    let mut f = fs::File::open(&path).expect("no file found");
+    let metadata = fs::metadata(&path).expect("unable to read metadata");
+    let buffer_length = if prefix_length > 0 { std::cmp::min(prefix_length as u64, metadata.len()) } else { metadata.len() as u64};
+    assert!(buffer_length <= std::usize::MAX as u64);
+    let mut buffer = Vec::new();
+    buffer.reserve_exact(buffer_length as usize);
 
-fn inverse(sa : &Vec<usize>) -> Vec<usize> {
-    let mut isa = vec![0; sa.len()];
-    for i in 0..sa.len() {
-        isa[sa[i]] = i as usize;
-    }
-    isa
-}
-
-
-
-fn duval(text: & ByteString) -> Vec<usize> {
-    let mut ending_positions = Vec::new();
-    let mut k = 0;
-    let n = text.len();
-    while k < n {
-        let mut i = k;
-        let mut j = k + 1;
-        while j != n && text[i] <= text[j] {
-            if text[i] < text[j] {
-                i = k;
-            }
-            if text[i] == text[j] {
-                i += 1;
-            }
-            j += 1;
-        }
-        loop {
-            assert_lt!(i,j);
-            k += j-i;
-            ending_positions.push(k-1 as usize);
-            if k >= i { break }
-        }
-    }
-    return ending_positions;
-}
-
-
-fn lyndonfactorization(isa : &Vec<usize>) -> Vec<usize> {
-    let mut ending_positions = Vec::new();
-    let mut k = 0;
-    let mut current_val = isa[k];
-    let n = isa.len();
-    k += 1;
-    while k < n {
-        if isa[k] < current_val {
-            ending_positions.push(k-1 as usize);
-            current_val = isa[k];
-        }
-        k += 1;
-    }
-    ending_positions.push(n-1);
-    return ending_positions;
-}
-
-// fn random_string_alpha(len : usize) -> ByteString {
-//     use rand::{thread_rng, Rng};
-//     use rand::distributions::Alphanumeric;
-//     return thread_rng().sample_iter(&Alphanumeric).take(len).map(u8::from).collect();
-// }
-
-#[allow(dead_code)]
-fn random_string_binary(len : usize) -> ByteString {
-    use rand::{thread_rng, Rng};
-    use rand::distributions::Open01;
-    return thread_rng().sample_iter(&Open01).take(len).map(|f : f32| -> u8 { return if f < 0.5 { 'a' as u8 } else { 'b' as u8}} ).collect();
-}
-
-
-
-#[test]
-fn test_duval() {
-    use std::cmp;
-    for length in 1..30 {
-        for _ in 0..cmp::min(1<<length,100) {
-            let text = random_string_binary(length);
-            let sa = suffixarray(&text);
-            let isa = inverse(&sa);
-            assert_eq!(duval(&text), lyndonfactorization(&isa));
-        }
-    }
-}
-
-use bit_vec::BitVec;
-const L_TYPE: bool = true;
-const S_TYPE: bool = false;
-
-
-fn s_star_positions(text : & ByteString, lyndon_factorization: & Vec<usize>) -> Vec<usize> {
-    let n = text.len();
-    let mut sl_types = BitVec::from_elem(n, S_TYPE);
-    let mut s_star_positions = Vec::new();
-    // sl_types.set(n-1, S_TYPE);
-    let mut current_lyndon_factor = lyndon_factorization.len()-1;
-    //@ invariant: sl_type[0] = S_TYPE
-    s_star_positions.push(n-1); //@ the last character has to be an S* type
-    for k in (1..n).rev() {
-        assert_lt!(k, n);
-        if k == lyndon_factorization[current_lyndon_factor] && k-1 == lyndon_factorization[current_lyndon_factor-1] { 
-            s_star_positions.push(k);
-            continue; //@ if our Lyndon factor consists only of one character, it is S-type
-        }
-        let next_character = 
-            if k != lyndon_factorization[current_lyndon_factor] { text[k+1] } 
-            else {
-            current_lyndon_factor -= 1;
-            text[lyndon_factorization[current_lyndon_factor]] };
-        if text[k] > next_character {
-            sl_types.set(k, L_TYPE);
-        } else if text[k] == next_character { 
-           assert_eq!(text[k+1], next_character); //@ if they are the same, then there is no way we wrap around since Lyndon factors have no borders. So k+1 is the index of `next_character`
-           sl_types.set(k, sl_types.get(k+1).unwrap()); 
-        }
-        if lyndon_factorization[current_lyndon_factor]+1 == k {
-            assert_eq!(S_TYPE, sl_types.get(k).unwrap());
-        }
-        if sl_types.get(k).unwrap() == S_TYPE && sl_types.get(k+1).unwrap() == L_TYPE {
-            s_star_positions.push(k);
-        }
-    }
-    s_star_positions.push(0); //@ the first character has to be an S* type
-    return s_star_positions;
-}
-
-fn lms_substrings(text : & ByteString, lyndon_factorization: & Vec<usize>, s_star_positions : & Vec<usize>) -> Vec<ByteString> {
-    let mut lms_substrings = Vec::new();
-    let mut current_lyndon_factor = 0;
-    for i in 0..s_star_positions.len() {
-        assert_ge!(s_star_positions[i], lyndon_factorization[current_lyndon_factor]);
-        if s_star_positions[i+1] > lyndon_factorization[current_lyndon_factor] {
-            let mut tail = text[s_star_positions[i]..lyndon_factorization[current_lyndon_factor]].to_vec();
-            // let a = vec!(*tail);
-            // tail.push('a' as u8);
-            tail.push(if current_lyndon_factor == 0 { text[0] } else { text[lyndon_factorization[current_lyndon_factor-1]+1] });
-            lms_substrings.push(ByteString::new(tail));
-            current_lyndon_factor += 1;
-        } else  {
-            lms_substrings.push(ByteString::new(text[s_star_positions[i]..s_star_positions[i+1]].to_vec()));
-        }
-    }
-    return lms_substrings;
-}
-
-fn bbwt(text : & ByteString, lyndon_factorization: & Vec<usize>) -> ByteString {
-    use succinct::BitVector;
-    use succinct::Rank9;
-    
-    let n = text.len();
-    let ending_positions = {
-        let mut v = BitVector::with_capacity(n);
-        for s in lyndon_factorization {
-            v[s] = true;
-        }
-        v
+    match f.read_to_end(&mut buffer) {
+        Ok(length) => assert_eq!(length, buffer.len()),
+        Err(x) =>  panic!(x)
     };
-    
-    let mut lyndon_starting_position = 0;
-    let mut current_lyndon_factor = 0;
-
-    let mut sa = vec![0; text.len()];
-    for i in 0..text.len() {
-        sa[i] = i as usize;
-    }
-    sa.sort_by(|a, b| {
-        let mut l = 0;
-        while 
-    }
-    // sa.sort_by_key(|x| { 
-    //     &text[*x..] 
-    // });
-    sa
+    buffer
 }
+
+#[derive(Debug,Clone)]
+struct StackElement {
+    text_pos : usize,
+    period : usize
+}
+
+fn successor_element(text : &[u8], start: usize, value: u8) -> Option<usize> {
+    if start >= text.len() {
+        return None;
+    }
+    let list = leftmost_distinct_characters(& text[start..]);
+    for el in list {
+        if text[start+el] >= value {
+            return Some(start+el);
+        }
+    }
+    None
+}
+
+fn leftmost_distinct_characters(text : &[u8]) -> Vec<usize> {
+    let mut charmap = std::collections::HashMap::new();
+    for i in 0..text.len() {
+        if charmap.contains_key(& text[i]) {
+            continue;
+        }
+        charmap.insert(text[i], i);
+    }
+    let mut list = Vec::new();
+    for el in charmap.values() {
+        list.push(*el);
+    }
+    list.sort_by(|a, b| text[*a].partial_cmp(& text[*b]).unwrap());
+    list
+}
+
+fn subsequence(text: &[u8], stack: &[StackElement]) -> Vec<u8> {
+    let mut subseq = Vec::new();
+    subseq.reserve_exact(stack.len());
+    for el in stack {
+        subseq.push(text[el.text_pos]);
+    }
+    if ! stack.is_empty() {
+        subseq.extend(stack.last().unwrap().period.to_string().as_bytes().into_iter());
+    }
+    subseq
+}
+
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -221,44 +78,57 @@ fn main() {
         eprintln!("Usage: {} [string]", args[0]);
         std::process::exit(1);
     }
-    let string : String = args[1].parse().unwrap();
-    let text = {
-        let mut msg = ByteString::from(string.as_bytes().to_vec());
-        msg[string.len()-1] = 0;
-        msg
-    };
+    let text = file2byte_vector(args[1].as_str(), 0);
 
-    let lyndon_factorization = duval(&text);
-    println!("{:?}", text);
-    println!("duval: {:?}", duval(&text));
-    let s_star_positions = s_star_positions(&text, &lyndon_factorization);
-    println!("s-stars: {:?}", s_star_positions);
-    println!("{:?}", lms_substrings(&text, &lyndon_factorization, &s_star_positions));
-
-
-    // println!("string length: {}", text.len());
-    // println!("duval: {:?}", duval(&text));
-    //
-    // let sa = suffixarray(&text);
-    // let isa = inverse(&sa);
-    // println!("lyndonfactorization: {:?}", lyndonfactorization(&isa));
-
+    let mut larray = Vec::new();
+    larray.resize(text.len(), usize::MAX);
     
+    let mut longest_lyndon_subsequence = Vec::new();
 
+    let mut stack = Vec::new();
+    let mut lastchildedgelabel = 0u8;
+    let mut upwardmove = false;
+    for starting_position in leftmost_distinct_characters(& text) { 
+        stack.push(StackElement { text_pos: starting_position, period : 1 });
+        while !stack.is_empty() {
+            let top = stack.last().unwrap();
+            let immature_character = text[stack[stack.len()-top.period as usize].text_pos];
+            let compare_char = if upwardmove { lastchildedgelabel } else { immature_character };
+            match successor_element(& text.as_slice(), top.text_pos+1, compare_char) {
+                None => {
+                    upwardmove = true;
+                    lastchildedgelabel = text[top.text_pos]+1;
+                    stack.pop(); () 
+                },
+                Some(i) => {
+                    assert_lt!(top.text_pos, i);
+                    assert_le!(compare_char, text[i]);
+                    let subsequence_length = stack.len()+1;
+                    // if false {
+                    if larray[subsequence_length] < i {
+                        upwardmove = true;
+                        lastchildedgelabel = text[i]+1;
+                        println!("{:?} + {} DISCARD", std::str::from_utf8(& subsequence(& text, & stack)).unwrap(), text[i]);
+                    }
+                    else {
+                        let new_period = 
+                            if immature_character == text[i] { top.period } else { subsequence_length };
 
-    // let args: Vec<String> = std::env::args().collect();
-    // if args.len() < 2 {
-    //     eprintln!("prints the i-th Thue-Morse word\nUsage: {} [number]", args[0]);
-    //     std::process::exit(1);
-    // }
-    // let index : u8 = args[1].parse().unwrap();
-    // use std::io::Write;
-    // std::io::stdout().write_all(thuemorse(index).as_slice()).unwrap();
-    // for i in 0..8 {
-        // let s : Vec<u8> = thuemorse(i).into_iter().map(|x| if x == 0 as u8 { 'a' as u8 } else { 'b' as u8 }).collect();
-        // let mut s = String::new();
-        // output_string(&mut s, & thuemorse(i)).unwrap();
-        // print!("{}", s);
-        // // println!("{} {:?}", i, thuemorse(i));
-    // }
+                        stack.push(StackElement { text_pos: i, period : new_period });
+                        if new_period == subsequence_length { //@ only update larray if we have a Lyndon subsequence
+                            larray[subsequence_length] = i;
+                            if longest_lyndon_subsequence.len() < subsequence_length {
+                                longest_lyndon_subsequence = stack.clone();
+                            }
+                        }
+                        upwardmove = false;
+                    }
+                }
+            }
+            println!("{:?} FOUND", std::str::from_utf8(& subsequence(& text, & stack)).unwrap());
+            // println!("{:?}", larray);
+        }
+    }
+    println!("{:?}", std::str::from_utf8(& subsequence(& text, & longest_lyndon_subsequence)));
+
 }
